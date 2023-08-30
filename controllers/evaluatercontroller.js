9a8d7d1c98767e94832bd2415e516d74b80c4dcb
Hamidaamir <hamidamir021@gmail.com>
2023-08-29 22:28:54 -0700

    updated evaluatercontroller

--- controllers/evaluatercontroller.js
+++ controllers/evaluatercontroller.js
@@ -3,14 +3,20 @@ const writingResult = require("../models/free_MockTest");
 
 const evaluateEssay = async (req, res) => {
   try {
-    const question_id = req.body.question_id;
-    const question_answer = req.body.question_answer;
+    const question_ids = req.body.question_id; // Assuming question_id is an array
+    const question_answers = req.body.question_answers;
 
-    const question = await writingResult.findById(question_id);
-    console.log(question.question_text);
+    // Now question_ids will contain ["64ba1117dfbf013551f50f90", "64ba1117dfbf013551f50f91", "64ba1117dfbf013551f50f92"]
+    
+    for (const id of question_ids) {
+      const question = await writingResult.findById(id);
+      console.log(question.question_text);
+      // You can process each question here
+    }
 
     res.status(200).json({ value: 10 });
   } catch (error) {
+    console.error(error); // Log the error to the console
     res.status(500).json({ error: "Internal server error" });
   }
 };
